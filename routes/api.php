7bb52f1e800cfb2f586f0c9b01c3c599cd564b56
Ygi Martin Santos <Ygisantos@gmail.com>
2025-05-01 11:23:35 +0800

    Refactor route comments for clarity: update public and protected route sections in api.php

--- routes/api.php
+++ routes/api.php
@@ -6,25 +6,26 @@ use App\Http\Controllers\api\AccountController;
 use App\Http\Controllers\api\StudentController;
 use App\Http\Controllers\api\CourseSubjectController;
 
-// Public routes
+// Auth
 Route::post('/register', [AccountController::class, 'register']);
 Route::post('/login', [AccountController::class, 'login']);
+
+// Enrollments
 Route::post('/student/create', [StudentController::class, 'store']);
 Route::put('/student/update', [StudentController::class, 'update']);
 Route::get('/student/get-all', [StudentController::class, 'index']);
 Route::get('/student/id', [StudentController::class, 'show']);
 
-// Course Subject routes
+// Pivot (Courses, Year Levels, Subjects)
 Route::get('/course-subject/get', [CourseSubjectController::class, 'index']);
 Route::get('/course-subject/get/id', [CourseSubjectController::class, 'show']);
 
-// Protected routes
 Route::middleware('auth:sanctum')->group(function () {
     Route::put('/account/update', [AccountController::class, 'update']);
     Route::post('/account/change-password', [AccountController::class, 'changePassword']);
     Route::put('/account/change-status', [AccountController::class, 'changeStatus']);
 
-    // Student routes
+    // Updates Enrollment
     Route::put('/student/update-status', [StudentController::class, 'updateStatus']);
     Route::delete('/student/delete', [StudentController::class, 'destroy']);
 });
